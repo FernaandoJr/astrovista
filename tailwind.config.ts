68a01512adbaa253dea40c01a49ecce7c6a0f39e
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-10-22 14:30:56 -0300

    Refactor tailwind.config.ts imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "tailwindcss"
 
-import svgToDataUri from "mini-svg-data-uri"
+const svgToDataUri = require("mini-svg-data-uri")
 
-import colors from "tailwindcss/colors"
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette"
+const colors = require("tailwindcss/colors")
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette")
 
 const config: Config = {
   darkMode: ["class"],
