621b227f1919e3ddc8cc3a8d815a16df2be25e5a
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-10-30 16:12:41 -0300

    updating the api return call

--- app/api/apod/route.ts
+++ app/api/apod/route.ts
@@ -2,21 +2,21 @@ import { NextResponse, NextRequest } from "next/server"
 import { getPictures } from "@/lib/mongo/pictures"
 
 export async function GET(req: NextRequest) {
-  // Get the start_date and end_date from the query parameters
-  let start_date = req.nextUrl.searchParams.get("start_date")
-  let end_date = req.nextUrl.searchParams.get("end_date")
+  // Use the NextRequest object to get URL parameters
+  const { searchParams } = new URL(req.url)
 
-  // If the start_date is not provided, set it to today
-  if (!start_date) {
-    start_date = new Date().toISOString().split("T")[0]
-  }
-  if (!end_date) {
-    end_date = start_date
-  }
+  // Get the start_date and end_date from the query string
+  // Example: /api/apod?start_date=2001-10-30&end_date=2001-11-30
 
-  // Get the pictures from the database
-  const { pictures, error } = await getPictures(start_date, end_date)
+  const startDate = searchParams.get("start_date")
+  const endDate = searchParams.get("end_date")
+
+  console.log("startDate:", startDate)
+  console.log("endDate:", endDate)
+
+  const { pictures, error } = await getPictures(startDate, endDate)
 
   if (error) throw new Error(error)
+
   return NextResponse.json({ pictures })
 }
