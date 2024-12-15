072b183cead8ea9a490f3efb60eb6bfe0aeaf469
Fernando Divino de Moraes Júnior <kinggprobr@gmail.com>
2024-12-15 17:57:26 -0300

    Add CORS support to gallery API; implement OPTIONS method for preflight requests

--- app/api/apod/gallery/route.ts
+++ app/api/apod/gallery/route.ts
@@ -1,12 +1,27 @@
 import { getGallery } from "@/lib/mongo/pictures"
 import { NextResponse, NextRequest } from "next/server"
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
+export async function GET(req: NextRequest){
     const { searchParams } = new URL(req.url)
     
     const page = parseInt(searchParams.get("page") ?? "", 10)
     const perPage = parseInt(searchParams.get("perPage") ?? "", 10)
 
     const gallery = await getGallery(perPage, page)
-    return NextResponse.json(gallery)
+
+    const response = NextResponse.json(gallery)
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+    return response
 }
+
+
+export async function OPTIONS() {
+    const response = NextResponse.json({})
+    response.headers.set("Access-Control-Allow-Origin", "*")
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    return response
+  }
