ec7a243eecc535ee660ccb8ef294ea5f2eef135d
Fernando Divino de Moraes Junior <kinggprobr@gmail.com>
2025-04-22 17:50:02 -0300

    updating and improving about-us page in mobile devices

--- components/astrovista/footer.tsx
+++ components/astrovista/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { SiGithub } from "@icons-pack/react-simple-icons"
+import { SiGithub, SiInstagram } from "@icons-pack/react-simple-icons"
 
 import { Separator } from "@/components/ui/separator"
 import PlanetLogo from "./planet-logo"
@@ -8,18 +8,21 @@ const description = "AstroVista is open-source web application that integrates i
 
 export default function Footer() {
   return (
-    <footer className="bg-background">
+    <footer className="mx-auto w-full bg-background md:container">
       <div className="mx-auto px-4 py-16 sx:mx-3 sx:px-3 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <div className="flex flex-wrap gap-10">
           <div>
             <div className="flex items-center">
               <PlanetLogo size={28} />
               <span className="ml-2 text-xl font-bold">AstroVista</span>
             </div>
             <p className="mt-4 max-w-xs text-sm text-muted-foreground">{description}</p>
-            <div className="mt-8 flex space-x-6 text-muted-foreground">
+            <div className="mt-6 flex gap-4 text-muted-foreground">
               <Link className="hover:text-primary" href="https://github.com/FernaandoJr/AstroVista" aria-label="Github">
                 <SiGithub className="h-6 w-6" />
+              </Link>
+              <Link className="hover:text-primary" href="https://instagram.com/astrovista.app" aria-label="Instagram">
+                <SiInstagram className="h-6 w-6" />
               </Link>
             </div>
           </div>
