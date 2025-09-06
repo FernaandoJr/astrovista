24fc828a4f68be2b22869dec5fe411895146ce54
Turbobot <turbobot@vercel.com>
2025-09-06 20:13:19 -0300

    feat(create-turbo): apply official-starter transform

--- apps/server/src/utils/isBoolean.ts
+++
@@ -1,3 +0,0 @@
-export function isBoolean(variable: any): boolean {
-	return typeof variable === "boolean";
-}
--- apps/server/src/utils/isValidDate.ts
+++
@@ -1,4 +0,0 @@
-export function isValidDateFormat(dateString: string): boolean {
-	const regex = /^\d{4}-\d{2}-\d{2}$/;
-	return regex.test(dateString);
-}
